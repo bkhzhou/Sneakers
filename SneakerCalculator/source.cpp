9794f1e07cdc70e6eb6ef3ee1d0334b7aec24c77
bkhzhou <79166987+bkhzhou@users.noreply.github.com>
2021-12-19 21:35:27 -0500

    Update source.cpp

--- SneakerCalculator/source.cpp
+++ SneakerCalculator/source.cpp
@@ -1,8 +1,7 @@
 /*StockX remodel of their seller fees.
 * StockX Level 1 is 13%, Level 2 12.5%, Level 3 12%, Level 4 11.5%
 * Goat has the same fee structure for sellers.
-* - Added new calculations fees
-* - Added "Fees" section to make your excel life easier.
+* - Added StockX minimum fees for accuracy selling under $100
 */
 
 #include<iostream>
@@ -17,7 +16,7 @@ int main()
 	double price, goat1, goat2 , stockx1, stockx2, stockx3, stockx4;
 
 	system("Color 0A");			//Able to change command sys color
-	cout << "Sneaker Calculator Goat / StockX made by KH#0870\nv0.05" << endl;
+	cout << "Sneaker Calculator Goat / StockX made by KH#0870\nv0.06" << endl;
 	cout << "Enter price for payout after fee: ";
 	cin >> price;
 	while (run)
@@ -25,11 +24,19 @@ int main()
 		cout << fixed << setprecision(2);
 		goat1 = (price * 0.095) + 5;		//Goat sale is 9.5% + $5 per
 		goat2 = (price - goat1) * 0.029;	//Goat cash out processing fee is 2.9%
-		stockx1 = (price * 0.13);			//StockX sale is its level % + 3% processing
-		stockx2 = (price * 0.125);
-		stockx3 = (price * 0.12);
-		stockx4 = (price * 0.115);
-		cout << endl;
+		if (price > 100) {					//StockX fee over 100
+			stockx1 = (price * 0.13);		//StockX sale is its level % + 3% processing
+			stockx2 = (price * 0.125);
+			stockx3 = (price * 0.12);
+			stockx4 = (price * 0.115);
+			cout << endl;
+		}
+		else {								//StockX minimum fee $9 + 3%
+			stockx1 = (price * 0.03) + 9;
+			stockx2 = (price * 0.03) + 9;
+			stockx3 = (price * 0.03) + 9;
+			stockx4 = (price * 0.03) + 9;
+		}
 
 		cout << left << setw(20) << "Goat Payout" << setw(20) << "StockX Level 1" << setw(20) << "Level 2" << setw(20) << "Level 3" << setw(20) << "Level 4" << endl;
 		cout << left << setw(20) << (price - goat1) - goat2 << setw(20) << price - stockx1 << setw(20) << price - stockx2 << setw(20) << price - stockx3 << setw(20) << price - stockx4 << endl;
